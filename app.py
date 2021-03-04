6254385896687b948286a04cf4be2f9fdc65a684
stevewlee123 <869073006@qq.com>
2021-03-03 21:17:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,6 @@ def index():
     return render_template('index.html')
 
 
-# def delete_folder(folder):
-#     if not os.listdir(folder):
-#         return
-#     for filename in os.listdir(folder):
-#         file_path = os.path.join(folder, filename)
-#         try:
-#             if os.path.isfile(file_path) or os.path.islink(file_path):
-#                 os.unlink(file_path)
-#             elif os.path.isdir(file_path):
-#                 shutil.rmtree(file_path)
-#         except Exception as e:
-#             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
 @app.route('/', methods=['POST'])
 def upload_file():
 
@@ -110,4 +97,4 @@ def move_forward():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run()
